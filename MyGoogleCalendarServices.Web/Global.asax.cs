529d989494cf0abd8c5814ae84323fb68fb9eef8
Daniel Tal <daniel.tal@outlook.com>
2016-05-08 02:43:28 +0300

    a

---
+++ MyGoogleCalendarServices.Web/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Http;
+
+namespace MyGoogleCalendarServices.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
